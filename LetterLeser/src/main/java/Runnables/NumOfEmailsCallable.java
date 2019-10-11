5aa38afacac7245b64a0a8efaaec19166aef9d56
UkraineBolt <alawrence2211@gmail.com>
2019-10-11 09:09:58 -0400

    Parallel stuff done up till running calculations

--- LetterLeser/src/main/java/Runnables/NumOfEmailsCallable.java
+++ LetterLeser/src/main/java/Runnables/NumOfEmailsCallable.java
@@ -1,8 +1,16 @@
 package Runnables;
 
+import edu.oswego.model.Email;
+
+import java.util.ArrayList;
 import java.util.concurrent.Callable;
 
 public class NumOfEmailsCallable implements Callable {
+    private static ArrayList<Email> emails;
+
+    public NumOfEmailsCallable(ArrayList<Email> emails){
+        this.emails = emails;
+    }
     @Override
     public Object call() throws Exception {
         return null;
--- LetterLeser/src/main/java/Runnables/SnRCallable.java
+++ LetterLeser/src/main/java/Runnables/SnRCallable.java
@@ -1,8 +1,16 @@
 package Runnables;
 
+import edu.oswego.model.Email;
+
+import java.util.ArrayList;
 import java.util.concurrent.Callable;
 
 public class SnRCallable implements Callable {
+    private static ArrayList<Email> emails;
+
+    public SnRCallable(ArrayList<Email> emails){
+        this.emails = emails;
+    }
     @Override
     public Object call() throws Exception {
         return null;
