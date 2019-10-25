47111fabef04b2922222d30bf73b2d79a4980a35
MikeDoran <mdoran@oswego.edu>
2019-10-24 21:00:03 -0400

    Added DomainCallable functionality and test

--- LetterLeser/src/main/java/edu/oswego/Runnables/DomainCallable.java
+++ LetterLeser/src/main/java/edu/oswego/Runnables/DomainCallable.java
@@ -1,7 +1,11 @@
 package edu.oswego.Runnables;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import edu.oswego.model.Email;
+import edu.oswego.model.EmailAddress;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.Callable;
 
@@ -14,6 +18,29 @@ public class DomainCallable implements Callable {
 
 	@Override
 	public Object call() throws Exception {
-		return null;
+		HashMap<String,Integer> domains = new HashMap<>();
+		for(Email e : emails){
+			List<EmailAddress> senders = e.getFrom();
+			for(EmailAddress ea : senders){
+				String domain = ea.getEmailAddress().split("@")[1];
+				Integer x = domains.putIfAbsent(domain, 1);
+				if (x != null) domains.replace(domain, ++x);
+			}
+		}
+
+		JsonObject emailsByDomain = new JsonObject();
+		JsonArray domainObjs = new JsonArray();
+		for(String domain : domains.keySet()){
+			JsonObject domainObj = new JsonObject();
+			JsonObject innerData = new JsonObject();
+			String [] domainMeta = domain.split("\\.");
+			innerData.addProperty("domainname", domainMeta[1]);
+			innerData.addProperty("domainparent", domainMeta[0]);
+			innerData.addProperty("contribution", domains.get(domain));
+			domainObj.add("domainobj",innerData);
+			domainObjs.add(domainObj);
+		}
+		emailsByDomain.add("emailbydomain", domainObjs);
+		return emailsByDomain;
 	}
 }
---
+++ LetterLeser/src/test/java/runables/DomainCallableTest.java
@@ -0,0 +1,65 @@
+package Runnables;
+
+import com.google.gson.JsonObject;
+import edu.oswego.Runnables.DomainCallable;
+import edu.oswego.model.Email;
+import edu.oswego.model.EmailAddress;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DomainCallableTest {
+
+    private List<Email> emails;
+
+    @BeforeEach
+    void setUp() {
+        emails = new ArrayList<>();
+        for(int i = 0; i < 10; i++){
+            List<EmailAddress> senders = new ArrayList<>();
+            senders.add(new EmailAddress(0, "mdoran@oswego.edu"));
+            Email e = new Email(0, null, "", 0, false, "", false, null, null, senders);
+            emails.add(e);
+        }
+        for(int i = 0; i < 9; i++){
+            List<EmailAddress> senders = new ArrayList<>();
+            senders.add(new EmailAddress(0, "mdoran@clarkson.edu"));
+            Email e = new Email(0, null, "", 0, false, "", false, null, null, senders);
+            emails.add(e);
+        }
+        for(int i = 0; i < 8; i++){
+            List<EmailAddress> senders = new ArrayList<>();
+            senders.add(new EmailAddress(0, "mdoran@yahoo.com"));
+            Email e = new Email(0, null, "", 0, false, "", false, null, null, senders);
+            emails.add(e);
+        }
+        for(int i = 0; i < 7; i++){
+            List<EmailAddress> senders = new ArrayList<>();
+            senders.add(new EmailAddress(0, "mdoran@something.dum"));
+            Email e = new Email(0, null, "", 0, false, "", false, null, null, senders);
+            emails.add(e);
+        }
+    }
+
+    @AfterEach
+    void tearDown(){ }
+
+    @Test
+    void testOutput() throws Exception{
+        DomainCallable dc = new DomainCallable(emails);
+        JsonObject returned = (JsonObject) dc.call();
+
+        String expected = "{\"emailbydomain\":[" +
+                "{\"domainobj\":{\"domainname\":\"dum\",\"domainparent\":\"something\",\"contribution\":7}}," +
+                "{\"domainobj\":{\"domainname\":\"com\",\"domainparent\":\"yahoo\",\"contribution\":8}}," +
+                "{\"domainobj\":{\"domainname\":\"edu\",\"domainparent\":\"clarkson\",\"contribution\":9}}," +
+                "{\"domainobj\":{\"domainname\":\"edu\",\"domainparent\":\"oswego\",\"contribution\":10}}" +
+                "]}";
+        assertEquals(returned.toString(),expected);
+    }
+}
