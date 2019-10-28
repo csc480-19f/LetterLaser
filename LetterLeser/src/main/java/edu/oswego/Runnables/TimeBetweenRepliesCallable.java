589e9e915301873486367c25c4533836af32c62e
PhoenixBoisnier <36176999+PhoenixBoisnier@users.noreply.github.com>
2019-10-28 17:21:15 -0400

    Converted return value into Json object.
    Formatted the return value to be a Json object.

--- LetterLeser/src/main/java/edu/oswego/Runnables/TimeBetweenRepliesCallable.java
+++ LetterLeser/src/main/java/edu/oswego/Runnables/TimeBetweenRepliesCallable.java
@@ -133,7 +133,19 @@ public class TimeBetweenRepliesCallable implements Callable {
 			times[1][q] /= totals[1][q];
 		}
 
-		return times;
+		JsonObject finalRet = new JsonObject();
+		JsonObject combined = new JsonObject();
+		JsonArray sent = new JsonArray();
+		JsonArray rece = new JsonArray();
+		for(int q = 0; q < 7; q++){
+			sent.add(times[0][q]);
+			rece.add(times[1][q]);
+		}
+		combined.add("SentEmails", sent);
+		combined.add("ReceivedEmails", rece);
+		finalRet.add("TimeBetweenReplies", combined);
+
+		return finalRet;
 	}
 
 	/**
