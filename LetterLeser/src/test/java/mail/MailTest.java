1df7ba69d5034ffa89427056589fc3adca96e41a
Conphucious <9phuc.nguyen6@gmail.com>
2019-10-29 20:55:02 -0400

    Mail testing

--- LetterLeser/src/test/java/mail/MailTest.java
+++ LetterLeser/src/test/java/mail/MailTest.java
@@ -2,6 +2,9 @@ package mail;
 
 import static org.junit.Assert.assertEquals;
 
+import javax.mail.Folder;
+import javax.mail.MessagingException;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -11,18 +14,17 @@ import edu.oswego.mail.Mailer;
 
 public class MailTest {
 	
-	Mailer mailer;
+	private Mailer mailer;
 	
 	@BeforeEach
 	void setUp() throws Exception {
 		Settings.loadCredentials();
-		mailer = new Mailer(edu.oswego.mail.Settings.EMAIL_ADDRESS, edu.oswego.mail.Settings.EMAIL_PWD);
-		
+	 	mailer = new Mailer(edu.oswego.mail.Settings.EMAIL_ADDRESS, edu.oswego.mail.Settings.EMAIL_PWD);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
-		mailer.getStorage().close();
+//		mailer.getStorage().close();
 	}
 	
 	@Test
@@ -34,6 +36,24 @@ public class MailTest {
 	void testConnectionFalse() {
 		mailer = new Mailer("fake@gmail.com", "fakepassword");
 		assertEquals(mailer.isConnected(), false);
+	}
+	
+	@Test 
+	void testFolderExists() throws MessagingException {
+		Folder folder = mailer.getFolder("CSC480_19F");
+		assertEquals(folder.exists(), true);
+	}
+	
+	@Test 
+	void testFolderNotExists() throws MessagingException {
+		Folder folder = mailer.getFolder("FAKE");
+		assertEquals(folder.exists(), false);
+	}
+	
+	@Test 
+	void testTotalEmailCount() {
+		int num = mailer.getTotalEmailCount();
+		assertEquals(num, 13692);
 	}
 
 }
