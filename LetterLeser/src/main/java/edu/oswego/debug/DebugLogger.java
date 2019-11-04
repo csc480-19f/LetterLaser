4d424d3d8ff2772da07da14c750522de0a06fc0a
Conphucious <9phuc.nguyen6@gmail.com>
2019-11-03 20:25:41 -0500

    yap

--- LetterLeser/src/main/java/edu/oswego/debug/DebugLogger.java
+++ LetterLeser/src/main/java/edu/oswego/debug/DebugLogger.java
@@ -6,10 +6,10 @@ import java.util.logging.*;
 This class is only for debug use. Will be removed at final deployment thus no tests required for this class
  */
 public class DebugLogger {
-    private static Logger logger;
-    private static FileHandler fh;
-    private static String piFilePath = "/home/csc480f19/LetterLeserLogFolder/LogFile.log";//use this only if you want to test on pi
-    private static String localFilePath = "D:\\logstuff\\logFile.log";//fill this for your local path if you want to debug on pc and replace piFilePath in filehandler constructor
+//    private static Logger logger;
+//    private static FileHandler fh;
+//    private static String piFilePath = "/home/csc480f19/LetterLeserLogFolder/LogFile.log";//use this only if you want to test on pi
+//    private static String localFilePath = "D:\\logstuff\\logFile.log";//fill this for your local path if you want to debug on pc and replace piFilePath in filehandler constructor
 
     public static void logEvent(String name, Level level,String event){
 //        logger = Logger.getLogger(name);
