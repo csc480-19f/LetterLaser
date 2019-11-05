b6f7d4bb8c94aee79abb61685cc751249ae0ed3a
PhoenixBoisnier <36176999+PhoenixBoisnier@users.noreply.github.com>
2019-11-04 19:23:05 -0500

    Updated sentiment analysis.
    Fixed pathing in SentimentByEmail.py to send files into sentiment folder and updated relevant paths in AnalyzeThis.java. Additionally, AnalyzeThis.java has been modified down to only the methods that will be used in the final portion of the program.

--- LetterLeser/src/main/java/edu/oswego/sentiment/AnalyzeThis.java
+++ LetterLeser/src/main/java/edu/oswego/sentiment/AnalyzeThis.java
@@ -4,7 +4,6 @@
 package edu.oswego.sentiment;
 
 import java.io.*;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -13,83 +12,28 @@ import java.util.Scanner;
 public class AnalyzeThis {
     private static final String pathToSentimentOnPi = "~/javaServer/apache-tomcat-9.0.26/webapps/LetterLeser/WEB-INF/classes/edu/oswego/sentiment/";
     //if we run into issues with other dir then we use the hard coded one above
-
-	private static SentimentScore[] process(String[] emails) {
-		return getScoresObjects(sentimize(emails));
-	}
-	private final static String pathToSentiment = System.getProperty("user.dir")+File.separator+"LetterLeser"+File.separator+"src"+File.separator+"main"+File.separator+"java"
+    private final static String pathToSentiment = System.getProperty("user.dir")+File.separator+"LetterLeser"+File.separator+"src"+File.separator+"main"+File.separator+"java"
             +File.separator+"edu"+File.separator+"oswego"+File.separator+"sentiment"+File.separator;
-    private final static File sentimentPerEmail = new File(pathToSentiment+"SentimentPerEmail.py");
     private final static File sentimentByEmail = new File(pathToSentiment+"SentimentByEmail.py");
-    private final static File sentenceCounter = new File(pathToSentiment+"SentenceCounter.py");
     private final static File output = new File(pathToSentiment+"output");
 
-
-    /**
-     * This returns the results from the sentiment analysis of the list of emails.
-     * @param emails The list of emails to be analyzed.
-     * @return A double[][][] array of emails' sentences' doubles for neg, neu, pos, and compound for the list of emails.
-     */
-    private static double[][][] sentimize(String[] emails){
-        ArrayList<ArrayList<ArrayList<Double>>> retVal = new ArrayList<>();
-        //This section feeds the python code the enciphered .txt file and demands the sentiment results.
-        String filePath = AnalyzeThis.encipher(emails);
-        File input = new File("output.txt");
-
-        try {
-            //TODO Replace the command string with the final pathname for the sentiment analyzer, SentimentPerEmail.py.
-            Process p = Runtime.getRuntime().exec("python C:/Users/pbois/PycharmProjects/Sentiment/SentimentPerEmail.py "
-                    + filePath);
-            BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()), 8);
-            String vals = in.readLine();
-            //TODO This will need to be changed to reflect the path the python file spits out, unless python puts it here.
-            while(!vals.equals("output.txt")){
-
-            }
-            Scanner scone = new Scanner(input);
-            while(scone.hasNext()){
-                Scanner cone = new Scanner(scone.nextLine());
-                ArrayList<ArrayList<Double>> scoresPerSentence = new ArrayList<>();
-                while(cone.hasNext()){
-                    ArrayList<Double> scores = new ArrayList<>();
-                    scores.add(Double.parseDouble(cone.next()));
-                    scores.add(Double.parseDouble(cone.next()));
-                    scores.add(Double.parseDouble(cone.next()));
-                    scores.add(Double.parseDouble(cone.next()));
-                    scoresPerSentence.add(scores);
-                }
-                retVal.add(scoresPerSentence);
-            }
-            p.destroy();
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-            System.out.println("Oh no.");
-        }
-
-        //Get your fresh, hot results here.
-
-        return AnalyzeThis.processResults(retVal);
-    }
+    //private final static File output = new File(pathToSentiment+"output");
 
     /**
      * This returns the results from the sentiment analysis of the list of emails.
      * @param emails The list of emails to be analyzed.
      * @return An array of emails' corresponding sentiment score objects.
      */
-    public static SentimentScore[] singleScoreSentimize(String[] emails){
+    public static SentimentScore[] process(String[] emails){
         SentimentScore[] scores = new SentimentScore[emails.length];
 
         //This section feeds the python code the enciphered .txt file and demands the sentiment results.
         String filePath = AnalyzeThis.encipher(emails);
 
         try {
-            //TODO Replace the command string with the final pathname for the whole sentiment analyzer, SentimentByEmail.py.
-            Process p = Runtime.getRuntime().exec("python C:/Users/pbois/PycharmProjects/Sentiment/SentimentByEmail.py "
-                    + filePath);
+            Process p = Runtime.getRuntime().exec("python " + sentimentByEmail.getPath() + " " + filePath);
             BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()), 8);
             String vals = in.readLine();
-            //TODO This will need to be changed to reflect the path the python file spits out, unless python puts it here.
             File input = new File(vals);
             Scanner scone = new Scanner(input);
             for(int q = 0; q < emails.length; q++){
@@ -120,7 +64,7 @@ public class AnalyzeThis {
     private static String encipher(String[] emails){
         //This section creates the file and enciphers it.
 
-        File pyIn = new File("pythonInput.txt");
+        File pyIn = new File(pathToSentiment+"pythonInput.txt");
         try{
             pyIn.createNewFile();
             FileWriter write = new FileWriter(pyIn);
@@ -146,92 +90,6 @@ public class AnalyzeThis {
     }
 
     /**
-     * Used to turn the return value of sentimize into an array.
-     * @param results These are the array list results to be processed.
-     * @return The double[][][] array.
-     */
-    private static double[][][] processResults(ArrayList<ArrayList<ArrayList<Double>>> results){
-        double[][][] retVal = new double[results.size()][][];
-        for(int q = 0; q < results.size(); q++){
-            retVal[q] = new double[results.get(q).size()][4];
-            for(int w = 0; w < results.get(q).size(); w++){
-                for(int e = 0; e < 4; e++){
-                    retVal[q][w][e] = results.get(q).get(w).get(e);
-                }
-            }
-        }
-        return retVal;
-    }
-
-    /**
-     * Feed me the results please.
-     * @param results Results from a processResults(sentimize(emails)) call.
-     * @return The average negative score.
-     */
-    public static double averageNeg(double[][][] results){
-        double total = 0.0;
-        double sentences = 0.0;
-        for(double[][] email : results){
-            for(double[] scores : email){
-                total += scores[0];
-                sentences += 1;
-            }
-        }
-        return total / sentences;
-    }
-
-    /**
-     * Feed me the results please.
-     * @param results Results from a processResults(sentimize(emails)) call.
-     * @return The average neutral score.
-     */
-    public static double averageNeu(double[][][] results){
-        double total = 0.0;
-        double sentences = 0.0;
-        for(double[][] email : results){
-            for(double[] scores : email){
-                total += scores[1];
-                sentences += 1;
-            }
-        }
-        return total / sentences;
-    }
-
-    /**
-     * Feed me the results please.
-     * @param results Results from a processResults(sentimize(emails)) call.
-     * @return The average positive score.
-     */
-    public static double averagePos(double[][][] results){
-        double total = 0.0;
-        double sentences = 0.0;
-        for(double[][] email : results){
-            for(double[] scores : email){
-                total += scores[2];
-                sentences += 1;
-            }
-        }
-        return total / sentences;
-    }
-
-    /**
-     * Feed me the results please.
-     * @param results Results from a processResults(sentimize(emails)) call.
-     * @return The average compound score.
-     */
-    public static double averageCompound(double[][][] results){
-        double total = 0.0;
-        double sentences = 0.0;
-        for(double[][] email : results){
-            for(double[] scores : email){
-                total += scores[3];
-                sentences += 1;
-            }
-        }
-        return total / sentences;
-    }
-
-    /**
      * This method restricts strings to char values 0 to 255.
      * @param s The string that needs to be fixed.
      * @return The fixed string.
@@ -245,52 +103,6 @@ public class AnalyzeThis {
                     retVal += letters[i];
                 }
             }
-        }
-        return retVal;
-    }
-
-    /**
-     * This method counts the number of sentences in an email. Probably easier to use this than the arrayList thing.
-     * @param email The text to be counted for sentences.
-     * @return The sentence count as an integer.
-     */
-    public static int sentenceCount(String email) {
-        int retVal = -1;
-        try {
-            //TODO change this to the final resting point for SentenceCounter.py
-            Process p =  Runtime.getRuntime().exec("python C:/Users/pbois/PycharmProjects/Sentiment/SentenceCounter.py "+email);
-            BufferedReader in = new BufferedReader(new InputStreamReader(p.getInputStream()), 8);
-            retVal = Integer.parseInt(in.readLine());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return retVal;
-    }
-
-    /**
-     * Feed me the results please.
-     * @param results Results from a processResults(sentimize(emails)) call.
-     * @return The list of compound scores.
-     */
-    private static SentimentScore[] getScoresObjects(double[][][] results){
-        SentimentScore[] retVal = new SentimentScore[results.length];
-        for(int q = 0; q < retVal.length; q++){
-            double negAvg = 0.0;
-            double neuAvg = 0.0;
-            double posAvg = 0.0;
-            double comAvg = 0.0;
-            for(int w = 0; w < results[q].length; w++){
-                negAvg += results[q][w][0];
-                neuAvg += results[q][w][1];
-                posAvg += results[q][w][2];
-                comAvg += results[q][w][3];
-            }
-            negAvg /= results[q].length;
-            neuAvg /= results[q].length;
-            posAvg /= results[q].length;
-            comAvg /= results[q].length;
-            SentimentScore s = new SentimentScore(negAvg, neuAvg, posAvg, comAvg);
-            retVal[q] = s;
         }
         return retVal;
     }
