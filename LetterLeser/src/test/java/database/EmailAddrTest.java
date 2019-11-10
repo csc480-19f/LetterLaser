39d10bcdee38f06ef4acae74db1bc7ee1a953374
45eer <48398853+45eer@users.noreply.github.com>
2019-11-09 19:30:47 -0500

    JUnit tests - emailaddrtest associated methods

--- LetterLeser/src/test/java/database/EmailAddrTest.java
+++ LetterLeser/src/test/java/database/EmailAddrTest.java
@@ -1,11 +1,20 @@
 package database;
 
 import edu.oswego.database.Database;
+
+import static org.junit.Assert.assertEquals;
+
+import java.sql.Date;
+import java.util.List;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import edu.oswego.database.Settings;
 import edu.oswego.mail.Mailer;
+import edu.oswego.model.Email;
+import edu.oswego.model.EmailAddress;
 
 /**
  * USE THIS AS A TEMPLATE
@@ -28,4 +37,84 @@ class EmailAddrTest {
 		db.truncateTables();
 	}
 
+	@Test
+	void testGetReceivedEmail() {
+		db.query("INSERT INTO email (subject) VALUES "
+				+ "('sub1');");
+		db.query("INSERT INTO email_addr (email_address) VALUES "
+				+ "('test@mail'),"
+				+ "('test2@mail'),"
+				+ "('test3@mail');");
+		db.query("INSERT INTO received_email (email_id, email_addr_id) VALUES "
+				+ "(1, 1),"
+				+ "(1, 2),"
+				+ "(1, 3);");
+		
+		
+		List<EmailAddress> emailListTest = db.getReceivedEmail(1);
+		assertEquals(emailListTest.size(), 3);
+	}
+	
+	@Test
+	void testGetReceivedEmailFail() {
+		List<EmailAddress> emailListTest = db.getReceivedEmail(1);
+		assertEquals(emailListTest.size(), 0);
+	}
+	
+	@Test
+	void testEmailExisterinoPistachinoCappucino() {
+		db.query("INSERT INTO email_addr (email_address) VALUE ('testerino@mail.com');");
+		String testMail = "testerino@mail.com";
+		assertEquals(db.emailAddressExists(testMail), true);
+	}
+	
+	@Test
+	void testEmailNoExisterinoPistachinoCappucino() {
+		db.query("INSERT INTO email_addr (email_address) VALUE ('testerino@mail.com');");
+		String testMail = "IAMNOTHERE@mail.com";
+		assertEquals(db.emailAddressExists(testMail), false);
+	}
+	
+	@Test
+	void testParseAddress() {
+		String testParse = db.parseAddress("test'123@gmail.com");
+		assertEquals(testParse.equals("test`123@gmail.com"), true);
+	}
+	
+	@Test
+	void testParseAddressFail() {
+		String testParse = db.parseAddress("test'123@gmail.com");
+		assertEquals(testParse.equals("test'123@gmail.com"), false);
+	}
+	
+	
+	@Test
+	void testReceivedEmailExists() {
+		db.query("INSERT INTO email (subject) VALUES "
+				+ "('sub1');");
+		db.query("INSERT INTO email_addr (email_address) VALUES "
+				+ "('test@mail'),"
+				+ "('test2@mail'),"
+				+ "('test3@mail');");
+		db.query("INSERT INTO received_email (email_id, email_addr_id) VALUES "
+				+ "(1, 1),"
+				+ "(1, 2),"
+				+ "(1, 3);");
+		assertEquals(db.receivedEmailExists(1, 1), true);
+	}
+	
+	@Test
+	void testReceivedEmailNoExists() {
+		db.query("INSERT INTO email (subject) VALUES "
+				+ "('sub1');");
+		db.query("INSERT INTO email_addr (email_address) VALUES "
+				+ "('test@mail'),"
+				+ "('test2@mail'),"
+				+ "('test3@mail');");
+		db.query("INSERT INTO received_email (email_id, email_addr_id) VALUES "
+				+ "(1, 1),"
+				+ "(1, 2),"
+				+ "(1, 3);");
+		assertEquals(db.receivedEmailExists(2, 1), false);
+	}
 }
