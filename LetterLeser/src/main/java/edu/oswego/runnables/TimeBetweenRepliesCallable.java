e14beb9619462dfaf3a84503592dbf3b812d501e
UkraineBolt <alawrence2211@gmail.com>
2019-11-16 20:21:35 -0500

    final intergration p1.4 fix timebetweenrepliescallable

--- LetterLeser/src/main/java/edu/oswego/runnables/TimeBetweenRepliesCallable.java
+++ LetterLeser/src/main/java/edu/oswego/runnables/TimeBetweenRepliesCallable.java
@@ -3,6 +3,9 @@ package edu.oswego.runnables;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import edu.oswego.model.Email;
+import org.joda.time.DateTime;
+import org.joda.time.Hours;
+import org.joda.time.format.DateTimeFormat;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -33,13 +36,14 @@ public class TimeBetweenRepliesCallable implements Callable {
 	 * 		indexes [x][1] are for Monday.
 	 * 		...
 	 * 		indexes [x][6] are for Saturday.
+     * 	    indexes [2][x] are total for sent
+     * 	    indexes [3][x] are total for recieve
 	 */
 	@Override
 	public Object call() throws Exception {
 		//The returned value, a 2-d array that stores the average time between sent at [0][x], and received at [1][x].
-		double[][] times = new double[2][7];
+		double[][] times = new double[4][7];
 		//This is the total number of emails sent, corresponding to the times in the array above.
-		double[][] totals = new double[2][7];
 		//This bit will save us time later.
 		ArrayList<String> keys = new ArrayList<>();
 
@@ -75,8 +79,35 @@ public class TimeBetweenRepliesCallable implements Callable {
 			}
 		}
 
-		//Here's where that bit from earlier is going to save us time.
+
 		for(String key : keys){
+            ArrayList<Email> emailThread = subjects.get(key);
+            for(int i=0;i<emailThread.size()-1;i++){
+                if(emailThread==null){continue;}
+                Email first = emailThread.get(i);
+                Email next = emailThread.get(i+1);
+                String sd = first.getDateReceived().toString();
+                String ed = next.getDateReceived().toString();
+                DateTime startDate = new DateTime(DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss").parseMillis(sd));
+                DateTime endDate = new DateTime(DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss").parseMillis(ed));
+                int hours = Hours.hoursBetween(startDate,endDate).getHours();
+                int dayOfWeek = getIndexOfDay(startDate.dayOfWeek().toString());
+                int sentOrRecieved;
+                //if sent else recieved
+                if(first.getFrom().get(0).getEmailAddress().equalsIgnoreCase(user)){
+                    sentOrRecieved = 0;
+                }else{
+                    sentOrRecieved = 1;
+                }
+
+                times[sentOrRecieved][dayOfWeek] = times[sentOrRecieved][dayOfWeek] + hours;
+                times[sentOrRecieved+2][dayOfWeek] = times[sentOrRecieved+2][dayOfWeek] + 1;
+
+            }
+
+        }
+		//Here's where that bit from earlier is going to save us time.
+		/*for(String key : keys){
 			ArrayList<Email> conversation = subjects.remove(key);
 
 			//We use this to check to see if the first email is from the user.
@@ -138,13 +169,25 @@ public class TimeBetweenRepliesCallable implements Callable {
 					}
 				}
 			}
-		}
+		}*/
 
 		//Here we just divide the total at each index and divide by its total count in totals to calculate the average.
-		for(int q = 0; q < 7; q++){
-			times[0][q] /= totals[0][q];
-			times[1][q] /= totals[1][q];
-		}
+		/*for(int q = 0; q < 7; q++){
+                times[0][q] /= totals[0][q];
+                times[1][q] /= totals[1][q];
+		}*/
+		for(int i=0;i<7;i++){
+		    if(times[2][i]!=0){
+                times[0][i] = times[0][i]/times[2][i];
+            }else{
+                times[0][i]=0;
+            }
+		    if(times[3][i]!=0){
+                times[1][i] = times[1][i]/times[3][i];
+            }else{
+                times[1][i]=0;
+            }
+        }
 
 		JsonObject combined = new JsonObject();
 		JsonArray sent = new JsonArray();
@@ -159,7 +202,26 @@ public class TimeBetweenRepliesCallable implements Callable {
 		return combined;
 	}
 
-	/**
+	private int getIndexOfDay(String day){
+	    if(day.equalsIgnoreCase("sunday")){
+	        return 0;
+        }else if(day.equalsIgnoreCase("monday")){
+            return 1;
+        }else if(day.equalsIgnoreCase("tuesday")){
+            return 2;
+        }else if(day.equalsIgnoreCase("wednesday")){
+            return 3;
+        }else if(day.equalsIgnoreCase("thursday")){
+            return 4;
+        }else if(day.equalsIgnoreCase("friday")){
+            return 5;
+        }else{//saturday
+            return 6;
+        }
+
+    }
+
+	/*/**
 	 * This method takes an email object and determines which day the email was
 	 * receive on.
 	 *
@@ -168,7 +230,7 @@ public class TimeBetweenRepliesCallable implements Callable {
 	 *            greater than 1-1-1970.
 	 * @return 0 for Sun, 1 for Mon, 2 for Tue, ... , 6 for Sat.
 	 */
-	private int findDay(Email e) {
+	/*private int findDay(Email e) {
 		double dayLong = e.getDateReceived().getTime() / oneDay;
 		// -3 shifts Sunday (3 % 7) to be (0 % 7) since (0 % 7) = Thursday normally.
 		return (((int) dayLong) - 3) % 7;
@@ -180,8 +242,8 @@ public class TimeBetweenRepliesCallable implements Callable {
 	 * @param end The email received / The email after the one sent.
 	 * @return The hour received as a number of hours, whose decimal is equivalent to the minutes.
 	 */
-	private double findHour(Email start, Email end){
+	/*private double findHour(Email start, Email end){
 		long diff = end.getDateReceived().getTime()-start.getDateReceived().getTime();
 		return diff / oneHour;
-	}
+	}*/
 }
