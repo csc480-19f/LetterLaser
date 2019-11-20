00feafca2ee42dbd2d48408a7fca65ca22d2b5af
MikeDoran <mdoran@oswego.edu>
2019-11-20 16:15:48 -0500

    Cleaned up folder callable and did some data management to fit GUI specs

--- LetterLeser/src/main/java/edu/oswego/runnables/FolderCallable.java
+++ LetterLeser/src/main/java/edu/oswego/runnables/FolderCallable.java
@@ -6,15 +6,15 @@ import edu.oswego.model.Email;
 
 import javax.mail.Folder;
 import javax.mail.MessagingException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.Callable;
 
 /**
- * @author PhoenixBoisnier This runnable calculates the folder populations. It
- *         assumes that the list of emails provided all exist within a folder
- *         within a folder, and assumes that all the emails in the relevant
+ * @author PhoenixBoisnier / MikeDoran
+ * 			This runnable calculates the folder populations. It
+ *         assumes that all the emails in the relevant
  *         folders is contained within the list given.
  */
 public class FolderCallable implements Callable {
@@ -25,94 +25,98 @@ public class FolderCallable implements Callable {
 	}
 
 	/**
-	 * This method creates a folderMapper class, which calculates the data for us in
-	 * the constructor.
+	 * 	Constructs and returns a JSON object containing a list of all folders, their parents,
+	 * 	and their content's contribution by number to the total number of given emails.
 	 */
 	@Override
-	public Object call() throws Exception {
+	public Object call() {
 		FolderMapper fm = new FolderMapper(emails);
-		HashMap<String, Integer> counts = fm.outerCount;
-		HashMap<String, String> relationships = fm.folderToParent;
-		ArrayList<String> keys = fm.keyRing;
 
-		JsonArray emailsByFolder = new JsonArray();
-		for (String s : keys) {
+		JsonObject emailsByFolder = new JsonObject();
+		JsonArray folderObjs = new JsonArray();
+		for (String folder : fm.allFolders()) {
 			JsonObject folderObj = new JsonObject();
-			JsonArray folderName = new JsonArray();
-			folderName.add(s);
-			folderObj.add("FolderName", folderName);
-			JsonArray folderParent = new JsonArray();
-			folderParent.add(relationships.get(s));
-			folderObj.add("FolderParent", folderParent);
-			JsonArray contributions = new JsonArray();
-			contributions.add(counts.get(s));
-			folderObj.add("Contributions", contributions);
-			emailsByFolder.add(folderObj);
+			JsonObject innerData = new JsonObject();
+
+			String folderParent = (fm.hasParent(folder))? fm.getParent(folder) : "0";
+
+			innerData.addProperty("foldername", folder);
+			innerData.addProperty("folderparent", folderParent);
+			innerData.addProperty("contribution", fm.getContribution(folder));
+			folderObj.add("folderobj", innerData);
+			folderObjs.add(folderObj);
 		}
+
+		emailsByFolder.add("emailbyfolder", folderObjs);
 		return emailsByFolder;
 	}
 
+
 	/**
-	 * This is the folderMapper class. It happened to be the way that made the most
-	 * sense to myself to organize the data. This class has protected results as:
-	 * outerCount; mapping the relevant folder names to their email counts,
-	 * folderToParent; mapping the relevant folders to their parent folder, and
-	 * keyRing; the complete list of keys used for outerCount and folderToParent.
+	 * FolderMapper: a book keeping class that keeps track of folder parents and
+	 * contributions.
 	 */
 	private class FolderMapper {
-		HashMap<String, Integer> outerCount = new HashMap<>();
-		ArrayList<String> keyRing = new ArrayList<>();
-		HashMap<String, String> folderToParent = new HashMap<>();
 
-		// The constructor also does the calculations.
-		FolderMapper(List<Email> emails) {
-			// For each email in emails,
-			for (Email e : emails) {
-				// Make a parent variable.
-				Folder parent = null;
-				try {
-					// The parent exists.
-					parent = e.getFolder().getFolder().getParent();
-				} catch (MessagingException ex) {
-					// Or the parent does not exist.
-					// This would disqualify an email based on the class assumptions.
-					System.out.println("Failed to locate parents.");
-					ex.printStackTrace();
-				}
-				// Since the parent exists,
-				if (parent != null) {
-					// We create a grandparent variable.
-					Folder grandParent = null;
-					// And do the same as above.
-					try {
-						grandParent = parent.getParent();
-					} catch (MessagingException ex) {
-						// Like above, an email that does not gave a grandparent does not meet our
-						// assumptions.
-						System.out.println("Failed to locate grandparents.");
-						ex.printStackTrace();
+		/**
+		 *	The constructor constructs a mapping of all folders to their parents,
+		 *	as well as a mapping of all folders to their contribution by number
+		 */
+		private HashMap<String,String> parents = new HashMap<>();
+		private HashMap<String, Integer> contribution = new HashMap<>();
+		public FolderMapper(List<Email> emails){
+			try {
+				for (Email e : emails) {
+					Folder current = e.getFolder().getFolder();
+					Folder parent;
+					parent = current.getParent();
+					while (parent.getName() != "") {
+						incrementKey(contribution, current.getName());
+						parents.put(current.getName(), parent.getName());
+						current = parent;
+						parent = current.getParent();
+
 					}
-					// Since the email matches our assumptions,
-					if (grandParent != null) {
-						// If the folder that our email is in has already been counted,
-						if (outerCount.containsKey(e.getFolder().getFolder().getName())) {
-							// Then we increment the value found.
-							outerCount.put(e.getFolder().getFolder().getName(),
-									outerCount.get(e.getFolder().getFolder().getName()) + 1);
-						}
-						// Otherwise, we make a new entry in our map.
-						else {
-							outerCount.put(e.getFolder().getFolder().getName(), 1);
-							// And since we only care about emails that get this far, we only store the
-							// folder info now.
-							keyRing.add(e.getFolder().getFolder().getName());
-						}
-						// In either case, since the parent exists, we'll add it to the relationship
-						// map.
-						folderToParent.put(e.getFolder().getFolder().getName(), parent.getName());
-					}
+					incrementKey(contribution, current.getName());
 				}
+			}catch(MessagingException e){
+				e.printStackTrace();
 			}
+		}
+
+		/**
+		 *	getParent: Gets The parent of a given folder (By name in String)
+		 */
+		public String getParent(String child){ return parents.get(child); }
+
+		/**
+		 *	getContribution: Gets The count of a given folder (By name in String)
+		 */
+		public int getContribution(String folder){ return contribution.get(folder); }
+
+		/**
+		 *	allFolders: Gets a set of all folders (By name in String)
+		 */
+		public Set<String> allFolders(){ return contribution.keySet(); }
+
+		/**
+		 *	hasParent: Returns true if a given folder (By name in String) has a parent
+		 */
+		public boolean hasParent(String folder){ return parents.containsKey(folder); }
+
+		/**
+		 *	incrementKey: Increments the value at a given key in a given HashMap,
+		 *	creating a new entry if one does not yet exist (Just to keep the
+		 *	constructor less cluttered)
+		 */
+		private HashMap<String, Integer> incrementKey(HashMap<String,Integer> map, String key) {
+			if(map.containsKey(key)){
+				int x = map.get(key);
+				map.put(key, ++x);
+			}else{
+				map.put(key, 1);
+			}
+			return map;
 		}
 	}
 }
