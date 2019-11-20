402c87d712d91a3bfb817d0f962f8b5c53713588
MikeDoran <mdoran@oswego.edu>
2019-11-19 20:40:22 -0500

    Fixed domain JSON and removed .idea folder

--- LetterLeser/src/test/java/runnables/DomainCallableTest.java
+++ LetterLeser/src/test/java/runnables/DomainCallableTest.java
@@ -1,5 +1,6 @@
 package runnables;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 
 import edu.oswego.model.Email;
@@ -18,50 +19,76 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class DomainCallableTest {
 
     private List<Email> emails;
+    private List<DomainObject> domainObjects;
 
     @BeforeEach
     void setUp() {
         emails = new ArrayList<>();
+        domainObjects = new ArrayList<>();
         for(int i = 0; i < 10; i++){
             List<EmailAddress> senders = new ArrayList<>();
             senders.add(new EmailAddress(0, "mdoran@oswego.edu"));
-            Email e = new Email(0, null, "", 0, false, false, null, null, senders);
+            Email e = new Email(0, null, "", 0, false, "", false, null, null, senders);
             emails.add(e);
         }
+        domainObjects.add(new DomainObject("edu","oswego", 10));
         for(int i = 0; i < 9; i++){
             List<EmailAddress> senders = new ArrayList<>();
-            senders.add(new EmailAddress(0, "mdoran@clarkson.edu"));
-            Email e = new Email(0, null, "", 0, false, false, null, null, senders);
+            senders.add(new EmailAddress(0, "mdoran@oswego.com"));
+            Email e = new Email(0, null, "", 0, false, "", false, null, null, senders);
             emails.add(e);
         }
+        domainObjects.add(new DomainObject("com","oswego", 9));
         for(int i = 0; i < 8; i++){
             List<EmailAddress> senders = new ArrayList<>();
-            senders.add(new EmailAddress(0, "mdoran@yahoo.com"));
-            Email e = new Email(0, null, "", 0, false, false, null, null, senders);
+            senders.add(new EmailAddress(0, "mdoran@oswego.org"));
+            Email e = new Email(0, null, "", 0, false, "", false, null, null, senders);
             emails.add(e);
         }
+        domainObjects.add(new DomainObject("org","oswego", 8));
         for(int i = 0; i < 7; i++){
             List<EmailAddress> senders = new ArrayList<>();
-            senders.add(new EmailAddress(0, "mdoran@something.dum"));
-            Email e = new Email(0, null, "", 0, false, false, null, null, senders);
+            senders.add(new EmailAddress(0, "mdoran@clarkson.edu"));
+            Email e = new Email(0, null, "", 0, false, "", false, null, null, senders);
             emails.add(e);
         }
+        domainObjects.add(new DomainObject("edu","clarkson", 7));
+        domainObjects.add(new DomainObject("oswego","0", 27));
+        domainObjects.add(new DomainObject("clarkson","0", 7));
     }
-
-    @AfterEach
-    void tearDown(){ }
 
     @Test
     void testOutput() throws Exception{
         DomainCallable dc = new DomainCallable(emails);
         JsonObject returned = (JsonObject) dc.call();
 
-        String expected = "{\"emailbydomain\":[" +
-                "{\"domainobj\":{\"domainname\":\"dum\",\"domainparent\":\"something\",\"contribution\":7}}," +
-                "{\"domainobj\":{\"domainname\":\"com\",\"domainparent\":\"yahoo\",\"contribution\":8}}," +
-                "{\"domainobj\":{\"domainname\":\"edu\",\"domainparent\":\"clarkson\",\"contribution\":9}}," +
-                "{\"domainobj\":{\"domainname\":\"edu\",\"domainparent\":\"oswego\",\"contribution\":10}}" +
-                "]}";
-        assertEquals(returned.toString(),expected);
+        JsonArray domains = returned.getAsJsonArray("emailbydomain");
+
+        assertEquals(domainObjects.size(), domains.size());
+        for(int i = 0; i < 6; i++){
+            JsonObject domain = domains.get(i).getAsJsonObject().get("domainobj").getAsJsonObject();
+
+            String domainName = domain.get("domainname").getAsString();
+            String domainParent = domain.get("domainparent").getAsString();
+            int contribution = domain.get("contribution").getAsInt();
+
+            for(DomainObject x : domainObjects){
+                if(x.name == domainName){
+                    assertEquals(x.parent, domainParent);
+                    assertEquals(x.cont, contribution);
+                }
+            }
+        }
+    }
+
+    private class DomainObject {
+        public String name;
+        public String parent;
+        public int cont;
+        public DomainObject(String n, String p, int c){
+            name = n;
+            parent = p;
+            cont = c;
+        }
     }
 }
