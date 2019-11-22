38a1aa1f01ee933056f9c9d5cdd0bf4a2b91c41b
MikeDoran <mdoran@oswego.edu>
2019-11-22 00:31:42 -0500

    Made Emails serializable and added FileManager class

---
+++ LetterLeser/src/main/java/edu/oswego/mail/FileManager.java
@@ -0,0 +1,64 @@
+import edu.oswego.model.Email;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileManager {
+
+    /**
+     * serializeFolder: Takes a List of emails and appends them to a file
+     * by the name [Email]-[Folder]. If no file by this name exists, then it
+     * is created. Assumes that the list contains emails of the same folder.
+     *
+     * @param user: The email address of the current user
+     * @param emails: The emails to be serialized
+     * @throws IOException
+     */
+    public void serializeFolder(String user, List<Email> emails) throws IOException{
+        File folder = new File("emails");
+        if(!folder.exists())folder.mkdir();
+
+        String imapFolder = emails.get(0).getFolder();
+
+        File f = new File("emails/" + user + "-" + imapFolder);
+        if(!f.exists()) f.createNewFile();
+        OutputStream os = new FileOutputStream(f,true);
+        os.flush();
+
+        for(Email email : emails) {
+            byte[] bytes = email.toBytes();
+            os.write(bytes.length);
+            os.write(bytes);
+        }
+
+        os.close();
+    }
+
+    /**
+     * deserializeFolder: deserializes and returns a list of emails from the given folder
+     * and given user email. Returns null if the specified folder doesn't exist within the
+     * filesystem.
+     *
+     * @param user: the email address of the current user
+     * @param imapFolder: the name of the folder to return
+     * @return a list of emails from the given folder
+     * @throws IOException
+     */
+    public List<Email> deserializeFolder(String user, String imapFolder) throws IOException {
+        File folder = new File("emails");
+        if(!folder.exists())return null;
+        File f = new File("emails/" + user + "-" + imapFolder);
+        if(!f.exists()) return null;
+
+        List<Email> emails = new ArrayList<>();
+        InputStream is = new FileInputStream(f);
+        while(is.available() != 0){
+            int length = is.read();
+            byte [] bytes = new byte[length];
+            is.read(bytes);
+            emails.add(new Email(bytes));
+        }
+        is.close();
+        return emails;
+    }
+}
--- LetterLeser/src/main/java/edu/oswego/model/Email.java
+++ LetterLeser/src/main/java/edu/oswego/model/Email.java
@@ -1,7 +1,10 @@
-package edu.oswego.model;
+import edu.oswego.model.EmailAddress;
+import edu.oswego.model.SentimentScore;
 
+import java.io.Serializable;
+import java.nio.ByteBuffer;
+import java.sql.Timestamp;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 /**
@@ -12,114 +15,210 @@ import java.util.List;
  *
  */
 
-public class Email {
+public class Email implements Serializable, Comparable {
 
-	private int id;
-	private Date dateReceived;
-	private String subject;
-	private double size;
-	private boolean isSeen;
-	private boolean hasAttachment;
+    private int id;
+    private Timestamp dateReceived;
+    private String subject;
+    private double size;
+    private boolean isSeen;
+    private boolean hasAttachment;
 
-	private SentimentScore sentimentScore;
-	private UserFolder folder;
-	private List<EmailAddress> from;
+    private SentimentScore sentimentScore;
+    private String folder;
+    private List<EmailAddress> from;
 
-	// TODO ADD USERFOLDER?
-	
-	public String getDate() {
-		return dateReceived.toString().substring(0, dateReceived.toString().length() - 2);
-	}
+    // TODO ADD USERFOLDER?
 
-	public Email(int id, Date dateReceived, String subject, double size, boolean isSeen, boolean hasAttachment,
-			UserFolder folder) {
-		this.id = id;
-		this.dateReceived = dateReceived;
-		this.subject = subject;
-		this.size = size;
-		this.isSeen = isSeen;
-		this.hasAttachment = hasAttachment;
-		this.sentimentScore = null;
-		this.folder = folder;
-		this.from = new ArrayList<>();
-	}
+    public Email(byte [] bytes){
+        ByteBuffer bb = ByteBuffer.wrap(bytes);
+        this.id = bb.getInt();
+        this.dateReceived = new Timestamp(bb.getLong());
+        int subjectLength = bb.getInt();
+        char [] subjectChars = new char[subjectLength];
+        for(int i = 0; i < subjectLength; i++){
+            subjectChars[i] = bb.getChar();
+        }
+        this.subject = new String(subjectChars);
+        this.size = bb.getDouble();
+        this.isSeen = (bb.getInt() == 1);
+        this.hasAttachment = (bb.getInt() == 1);
+        double pos = bb.getDouble();
+        double neg = bb.getDouble();
+        double neu = bb.getDouble();
+        double cpd = bb.getDouble();
+        this.sentimentScore = new SentimentScore(pos,neg,neu,cpd);
+        int folderLength = bb.getInt();
+        char [] folderChars = new char[folderLength];
+        for(int i = 0; i < folderLength; i++){
+            folderChars[i] = bb.getChar();
+        }
+        this.folder = new String(folderChars);
+        this.from = new ArrayList<>();
+        int fromSize = bb.getInt();
+        for(int i = 0; i < fromSize; i++){
+            int id = bb.getInt();
+            int emailLength = bb.getInt();
+            char [] emailChars = new char[emailLength];
+            for(int j = 0; j < emailLength; j++){
+                emailChars[i] = bb.getChar();
+            }
+            String email = new String(emailChars);
+            from.add(new EmailAddress(id,email));
+        }
+    }
 
-	public Email(int id, Date dateReceived, String subject, double size, boolean isSeen, boolean hasAttachment,
-			SentimentScore sentimentScore) {
-		this.id = id;
-		this.dateReceived = dateReceived;
-		this.subject = subject;
-		this.size = size;
-		this.isSeen = isSeen;
-		this.hasAttachment = hasAttachment;
-		this.sentimentScore = sentimentScore;
-		this.from = new ArrayList<>();
-	}
+    public Email(int id, Timestamp dateReceived, String subject, double size, boolean isSeen, boolean hasAttachment,
+                 String folder) {
+        this.id = id;
+        this.dateReceived = dateReceived;
+        this.subject = subject;
+        this.size = size;
+        this.isSeen = isSeen;
+        this.hasAttachment = hasAttachment;
+        this.sentimentScore = null;
+        this.folder = folder;
+        this.from = new ArrayList<>();
+    }
 
-	public Email(int id, Date dateReceived, String subject, double size, boolean isSeen, boolean hasAttachment,
-			SentimentScore sentimentScore, UserFolder folder) {
-		this.id = id;
-		this.dateReceived = dateReceived;
-		this.subject = subject;
-		this.size = size;
-		this.isSeen = isSeen;
-		this.hasAttachment = hasAttachment;
-		this.sentimentScore = sentimentScore;
-		this.folder = folder;
-		this.from = new ArrayList<>();
-	}
+    public Email(int id, Timestamp dateReceived, String subject, double size, boolean isSeen, boolean hasAttachment,
+                 SentimentScore sentimentScore) {
+        this.id = id;
+        this.dateReceived = dateReceived;
+        this.subject = subject;
+        this.size = size;
+        this.isSeen = isSeen;
+        this.hasAttachment = hasAttachment;
+        this.sentimentScore = sentimentScore;
+        this.from = new ArrayList<>();
+    }
 
-	public Email(int id, Date dateReceived, String subject, double size, boolean isSeen, boolean hasAttachment,
-			SentimentScore sentimentScore, UserFolder folder, List<EmailAddress> from) {
-		this.id = id;
-		this.dateReceived = dateReceived;
-		this.subject = subject;
-		this.size = size;
-		this.isSeen = isSeen;
-		this.hasAttachment = hasAttachment;
-		this.sentimentScore = sentimentScore;
-		this.folder = folder;
-		this.from = from;
-	}
+    public Email(int id, Timestamp dateReceived, String subject, double size, boolean isSeen, boolean hasAttachment,
+                 SentimentScore sentimentScore, String folder) {
+        this.id = id;
+        this.dateReceived = dateReceived;
+        this.subject = subject;
+        this.size = size;
+        this.isSeen = isSeen;
+        this.hasAttachment = hasAttachment;
+        this.sentimentScore = sentimentScore;
+        this.folder = folder;
+        this.from = new ArrayList<>();
+    }
 
-	public int getId() {
-		return id;
-	}
+    public Email(int id, Timestamp dateReceived, String subject, double size, boolean isSeen, boolean hasAttachment,
+                 SentimentScore sentimentScore, String folder, List<EmailAddress> from) {
+        this.id = id;
+        this.dateReceived = dateReceived;
+        this.subject = subject;
+        this.size = size;
+        this.isSeen = isSeen;
+        this.hasAttachment = hasAttachment;
+        this.sentimentScore = sentimentScore;
+        this.folder = folder;
+        this.from = from;
+    }
 
-	public Date getDateReceived() {
-		return dateReceived;
-	}
+    public String getTimestamp() {
+        return dateReceived.toString().substring(0, dateReceived.toString().length() - 2);
+    }
 
-	public String getSubject() {
-		return subject;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public double getSize() {
-		return size;
-	}
+    public Timestamp getDateReceived() {
+        return dateReceived;
+    }
 
-	public boolean isSeen() {
-		return isSeen;
-	}
+    public String getSubject() {
+        return subject;
+    }
 
-	public boolean hasAttachment() {
-		return hasAttachment;
-	}
-	
-	public void setSentimentScore(SentimentScore sentimentScore) {
-		this.sentimentScore = sentimentScore;
-	}
+    public double getSize() {
+        return size;
+    }
 
-	public SentimentScore getSentimentScore() {
-		return sentimentScore;
-	}
+    public boolean isSeen() {
+        return isSeen;
+    }
 
-	public UserFolder getFolder() {
-		return folder;
-	}
+    public boolean hasAttachment() {
+        return hasAttachment;
+    }
 
-	public List<EmailAddress> getFrom() {
-		return from;
-	}
+    public SentimentScore getSentimentScore() {
+        return sentimentScore;
+    }
 
+    public String getFolder() {
+        return folder;
+    }
+
+    public List<EmailAddress> getFrom() {
+        return from;
+    }
+
+    private int calculateNumBytes(){
+        int subjectSize = 4 + (2 * subject.length()); // for length (int) and 2*length (chars)
+        int folderSize = 4 + (2 * folder.length()); // for length (int) and 2*length (chars)
+        int sentimentScoreSize = 8*4; //4 doubles
+        int fromSize = 4; //int for the length of the list
+        for(EmailAddress ea : from){
+            fromSize += 8; //for the id and length (both int)
+            fromSize += (2 * ea.getEmailAddress().length());
+        }
+
+        return 4 // id (int)
+                + 8 // dateReceived (TimeStamp, can be constructed with long)
+                + subjectSize
+                + 8 // size (double)
+                + 4 // isSeen (boolean, stored as int)
+                + 4 // hasAttachment (boolean, stored as int)
+                + sentimentScoreSize
+                + folderSize
+                + fromSize;
+    }
+
+    public byte[] toBytes(){
+        int numBytes = calculateNumBytes();
+        ByteBuffer bb = ByteBuffer.wrap(new byte[numBytes]);
+
+        bb.putInt(id);
+        bb.putLong(dateReceived.getTime());
+        bb.putInt(subject.length());
+        char [] subjectChars = subject.toCharArray();
+        for(char c : subjectChars) bb.putChar(c);
+        bb.putDouble(size);
+        bb.putInt((isSeen)? 1 : 0);
+        bb.putInt((hasAttachment)? 1 : 0);
+        bb.putDouble(sentimentScore.getPositive());
+        bb.putDouble(sentimentScore.getNegative());
+        bb.putDouble(sentimentScore.getNeutral());
+        bb.putDouble(sentimentScore.getCompound());
+        bb.putInt(folder.length());
+        char [] folderChars = folder.toCharArray();
+        for(char c : folderChars) bb.putChar(c);
+        bb.putInt(from.size());
+        for(EmailAddress ea : from){
+            bb.putInt(ea.getId());
+            bb.putInt(ea.getEmailAddress().length());
+            char [] addressChars = ea.getEmailAddress().toCharArray();
+            for(char c : addressChars) bb.putChar(c);
+        }
+
+        return bb.array();
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        Email e = (Email) o;
+        if(this.getDateReceived().getTime() > e.getDateReceived().getTime()) {
+            return 1;
+        }else if(this.getDateReceived().getTime() == e.getDateReceived().getTime()) {
+            return 0;
+        }else {
+            return -1;
+        }
+    }
 }
